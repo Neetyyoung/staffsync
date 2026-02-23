a911ad9acf49c61749344dd3c8ac8ab8087be37a
Neeyyoung <keshlynwalker@gmail.com>
2026-02-23 01:53:53 +0000

    Removed hardcoded DB credentials

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Root123!",
-  database: "attendance_system"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
+    console.error("Database connection failed ❌:", err);
   } else {
     console.log("Connected to MySQL database ✅");
   }
